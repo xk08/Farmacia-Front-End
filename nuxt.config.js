e5e855f4084ac6175c75a3bbd4577f7d16722b6e
Marcos Vinnicius Martins <gronytzki@gmail.com>
2020-05-11 11:56:20 -0300

    Incluindo mecanismo do GET dos Remédios

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,5 +52,5 @@ export default {
     */
     extend (config, ctx) {
     }
-  }
+  },
 }
--- pages/index.vue
+++ pages/index.vue
@@ -12,13 +12,13 @@
       <b-col>
         <div class="styles colorContent">
           <Drug
-            v-for="teste in drugs"
-            :key="teste.id"
-            :title="teste.title"
-            :image="teste.image"
-            :qtd="teste.qtd"
-            :id="teste.id"
-            :value="teste.value"
+            v-for="item in drugs"
+            :key="item.id"
+            :title="item.title"
+            :image="item.image"
+            :qtd="item.amount"
+            :id="item.id"
+            :value="item.value"
           />
         </div>
       </b-col>
@@ -34,6 +34,7 @@
 
 <script>
 import Drug from "@/components/Drug";
+import axios from "axios";
 
 export default {
   components: {
@@ -44,56 +45,19 @@ export default {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         resolve({
-          drugs: [
-            {
-              id: "1",
-              title: "Dipirona",
-              value: "100,00",
-              qtd: "8",
-              image:
-                "https://www.sansilfarma.com.br/BACKOFFICE/Uploads/Produto/Ampliada/237818.png"
-            },
-            {
-              id: "2",
-              title: "Paracetamol",
-              value: "80,00",
-              qtd: "10",
-              image:
-                "https://static-webv8.jet.com.br/drogaosuper/Produto/multifotos/hd/5409032017091306.jpg"
-            },
-
-            {
-              id: "3",
-              title: "Benzetacil",
-              value: "200,00",
-              qtd: "50",
-              image:
-                "https://t2.uc.ltmcdn.com/pt/images/5/1/2/img_benzetacil_indicacoes_e_efeitos_colaterais_17215_600_square.jpg"
-            },
-
-            {
-              id: "4",
-              title: "Buscopan",
-              value: "20,00",
-              qtd: "1",
-              image:
-                "https://www.farmadelivery.com.br/media/catalog/product/cache/1/image/720x/9df78eab33525d08d6e5fb8d27136e95/4/6/4633-buscopan-composto-adulto-10mg-250mg-c-20-comprimidos.jpg"
-            },
-
-            {
-              id: "5",
-              title: "Ibuprofeno",
-              value: "75,00",
-              qtd: "2",
-              image:
-                "https://drogariavenancio.vteximg.com.br/arquivos/ids/563624-1000-1000/62897_Z.jpg?v=636966115928530000"
-            }
-          ]
+          drugs: []
         });
       }, 1500);
     });
-  }
-};
+  },
+
+    mounted() {
+        axios.get('http://localhost:8080/drugs')
+        .then(response =>{
+            this.drugs = response.data;
+        })
+    }
+}
 </script>
 
 <style scoped>
